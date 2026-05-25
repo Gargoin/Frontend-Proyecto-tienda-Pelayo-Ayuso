055fae1a9ee3de2327a190224d0ff9c90bdaa7a9
Pelayo Ayuso <pelayo.ayuso@gmail.com>
2026-05-25 20:33:30 +0200

    Hecho ProductList

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,12 @@ function Navbar () {
 
     return (
         <nav className="navbar container">
+            <div className="menu">
             <a href="#">[Home]</a>
+            <a href="#">[Textil]</a>
+            <a href="#">[Prints]</a>
+            </div>
+
             <div className="login">
             <a href="#">[Crear cuenta]</a>
             <a href="#">[Login]</a>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,38 @@
+import ProductList from "../components/ProductList"
 import{productos} from "../data/productos";
-import ProductCard from "../components/ProductCard";
 
 function Home () {
+
+    const productosDestacados = productos.filter(producto => producto.destacado);
+    const nuevosProductos = productos.slice(0,3);
+
     return (
     <main>
+
         <section className="hero">
             <div className="container">
                 <span className="hero-label">Proyecto</span>
-                    <h1>The Doomgeon Project</h1>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque alias eveniet autem odit tempora. Amet nisi repudiandae id sed quos, dolore natus reprehenderit adipisci aliquam quas laborum et porro illum?</p>
-                    <a className="button" href="#"><span>Ver productos</span></a>
+                <h1>The Doomgeon Project</h1>
+                <p>TEXTO DEL HERO - Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque alias eveniet autem odit tempora. Amet nisi repudiandae id sed quos, dolore natus reprehenderit adipisci aliquam quas laborum et porro illum?</p>
+                <a className="button" href="#"><span>Ver productos</span></a>
             </div>
         </section>
+
+        <section className="productos-nuevos">
+            <div className="container">
+                <h2>Nuevos productos</h2>
+                <ProductList productos={nuevosProductos}/>
+            </div>
+        </section>
+
+
         <section className="productos-destacados">
             <div className="container">
                 <h2>Contenido destacado</h2>
-                <div className="lista-productos">
-                    {productos.slice(0,3).map(producto => (
-                        <ProductCard key={producto.id} producto={producto}/>
-                    ))}
-                </div>
+                <ProductList productos={productosDestacados}/>
             </div>
-
         </section>
+       
 
     </main>
     )
